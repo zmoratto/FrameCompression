640f30e6310bdca0403cff294804d4866e8a25a7
Zack Moratto <zachary.m.moratto@nasa.gov>
2013-10-30 21:32:23 -0700

    Change depth encoding so only store 14 bits.
    We should be more immune to single DN changes in the depth map.

--- depth_code.c
+++ depth_code.c
@@ -9,7 +9,7 @@
 
 void depth_to_yuv(uint16_t depth, uint8_t *y, uint8_t *u) {
   uint16_t u0;
-  *y = (depth >> 8 );
+  *y = (depth >> 6 & 0xFF);
   u0 = depth % NP;
   if ( u0 >= HNP ) {
     *u = NP - u0 - 1;
@@ -20,10 +20,11 @@ void depth_to_yuv(uint16_t depth, uint8_t *y, uint8_t *u) {
 
 void yuv_to_depth(uint8_t y, uint8_t u, uint16_t *depth) {
   *depth = y;
-  *depth <<= 8;
-  if ( y % 2 ) {
-    *depth += (255 - u);
+  *depth <<= 6;
+  *depth &= 0xFF00;
+  if ( y & 0x04 ) {
+    *depth |= (255 - u);
   } else {
-    *depth += u;
+    *depth |= u;
   }
 }
